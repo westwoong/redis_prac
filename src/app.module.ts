6c69fc3c04d6c1acba66953cb9d029ef3eb2442e
westwoong <skyaliceg@naver.com>
2025-05-30 15:20:31 +0900

    users cache create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { CacheModule } from './cache/cache.module';
+import { RedisModule, RedisModuleOptions } from "@nestjs-modules/ioredis";
 
 @Module({
-  imports: [],
-  providers:[],
+  imports: [
+    UsersModule,
+    CacheModule,
+    RedisModule.forRootAsync({
+      useFactory: (): RedisModuleOptions => ({
+        type: 'single',
+        options: {
+          host: 'localhost',
+          port: 6379,
+          db: 2,
+        },
+      }),
+    }),
+  ],
+  providers: [],
 })
 export class AppModule {
 }
