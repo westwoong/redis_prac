bae5857524d18ee3778dc59e7a90daa0c5ac0475
westwoong <skyaliceg@naver.com>
2025-06-24 15:17:56 +0900

    ranking 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { RedisModule } from './cache/redis.module';
+import { RankingModule } from './ranking/ranking.module';
 
 @Module({
   imports: [
     UsersModule,
     RedisModule,
+    RankingModule,
   ],
   providers: [],
 })
---
+++ src/ranking/ranking.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { RankingService } from './ranking.service';
+import { RedisModule } from '../cache/redis.module';
+import { PrismaModule } from '../orm/prisma.module';
+
+@Module({
+  imports: [
+    RedisModule,
+    PrismaModule,
+  ],
+  providers: [
+    RankingService,
+  ],
+  exports: [
+    RankingService,
+  ],
+})
+export class RankingModule {} 
