cfade9999c571a4a94795129b6a85f1e02e07e83
westwoong <skyaliceg@naver.com>
2025-06-24 16:06:35 +0900

    rankingService.addScore() 테스트

--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import Redis from 'ioredis';
 import { PrismaService } from '../orm/prisma.service';
 import { ScoreRequestDto } from "../score/dto/score.req.dto";
@@ -15,6 +15,13 @@ export class RankingService {
 
   async addScore(data: ScoreRequestDto): Promise<void> {
     const { score, userId } = data;
+    const isExistUser = await this.prismaService.users.findUnique({ where: { id: userId } });
+    if (isExistUser) throw new NotFoundException();
+    try {
+      await this.prismaService.dailyRanking.create({ data: { userId: userId, score: score } });
+    } catch (error) {
+      this.logger.error(error);
+    }
     this.logger.debug(`점수 추가 : ${ score } 사용자 ${ userId }`);
   }
 
