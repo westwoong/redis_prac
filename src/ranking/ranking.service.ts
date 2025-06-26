8984f33e07a819b1184428cf5eeccbb7fa1f4de6
westwoong <skyaliceg@naver.com>
2025-06-26 12:20:19 +0900

    rankingService DB저장, 조회 로직 테스트(redis 실패 시)

--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -17,29 +17,85 @@ export class RankingService {
     const { score, userId } = data;
     const isExistUser = await this.prismaService.users.findUnique({ where: { id: userId } });
     if (!isExistUser) throw new NotFoundException();
+
     const today = this.formatDate(new Date());
     const redisDailyRankingKey = `userRanking:daily:${ today }`;
 
     try {
       await this.redisService.zset(redisDailyRankingKey, score, userId);
+      this.logger.debug(`redis - 사용자: ${ userId }, 점수: ${ score }`);
     } catch (error) {
-      this.logger.error(error);
+      this.logger.error('redis 실패:', error);
     }
-    this.logger.debug(`키 : ${ redisDailyRankingKey } 점수 추가 : ${ score } 사용자 ${ userId }`);
+
+    try {
+      await this.saveScoreToDb(userId, score, new Date());
+      this.logger.debug(`db save 사용자: ${ userId }, 점수: ${ score }`);
+    } catch (error) {
+      this.logger.error('db 실패:', error);
+    }
   }
 
   async getRankingDesc(userId: string) {
+    const userIdNum = parseInt(userId);
     const today = this.formatDate(new Date());
     const redisDailyRankingKey = `userRanking:daily:${ today }`;
+
     try {
-      const result = await this.redisService.zGetDesc(redisDailyRankingKey, userId);
-      return {
-        rank: result.rank !== null ? result.rank + 1 : '집계중',
-        score: result.score ? parseFloat(result.score) : '집계중'
-      };
+      const redisResult = await this.redisService.zGetDesc(redisDailyRankingKey, userIdNum);
+      if (redisResult.rank !== null && redisResult.score) {
+        this.logger.debug(`[redis 조회] ${ redisResult }`);
+        return {
+          rank: redisResult.rank + 1, // 1 더해줘야함 배열같이 0부터 시작함
+          score: parseFloat(redisResult.score)
+        };
+      }
     } catch (error) {
-      this.logger.error(error);
+      this.logger.error('redis 랭킹 조회 실패:', error);
     }
+
+    try {
+      const todayDate = new Date();
+      const dbRanking = await this.getDbRanking(userIdNum, todayDate);
+      if (dbRanking) {
+        return {
+          rank: dbRanking.rank ? dbRanking.rank : '집계중',
+          score: dbRanking.score
+        }
+      }
+    } catch (error) {
+      this.logger.error('db 랭킹 조회 실패:', error);
+    }
+
+    this.logger.debug('redis&db 둘 다 데이터 없음');
+    return { rank: '미포함', score: '미포함' };
+  }
+
+  private async saveScoreToDb(userId: number, score: number, date: Date) {
+    const userCheck = await this.prismaService.users.findUnique({ where: { id: userId } });
+    if (!userCheck) throw new NotFoundException();
+
+    const dateOnly = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    const existingRanking = await this.prismaService.dailyRanking.findFirst({ where: { userId, date: dateOnly } });
+
+    // 랭킹 있으면 점수 추가
+    if (existingRanking) {
+      await this.prismaService.dailyRanking.update({
+        where: { id: existingRanking.id },
+        data: { score: existingRanking.score + score }
+      });
+    } else {
+      await this.prismaService.dailyRanking.create({ data: { userId, score, date: dateOnly } }); // 없으면 새로 생성
+    }
+  }
+
+
+  private async getDbRanking(userId: number, date: Date) {
+    const dateOnly = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+    const userRanking = await this.prismaService.dailyRanking.findFirst({ where: { userId, date: dateOnly } });
+    this.logger.debug(`[redis 실패 - db 조회 시작] ${ userRanking }`);
+    return userRanking;
   }
 
   private formatDate(date: Date) { // YYYY-MM -DD
