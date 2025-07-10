7d022ee535d9da51345e475d3a423d7aa228fcf2
westwoong <skyaliceg@naver.com>
2025-07-10 14:10:29 +0900

    Feat : mongoose 모듈설정 app모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,32 @@ import { UsersModule } from './users/users.module';
 import { RedisModule } from './cache/redis.module';
 import { RankingModule } from './ranking/ranking.module';
 import { ScoreModule } from "./score/score.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { LoggerModule } from "./common/logger/logger.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     UsersModule,
     RedisModule,
     RankingModule,
     ScoreModule,
+    LoggerModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const mongoUrl = configService.get<string>('MONGODB_URL');
+        if (!mongoUrl) {
+          return { uri: 'fail' };
+        }
+        return { uri: mongoUrl };
+      },
+    }),
   ],
   providers: [],
 })
