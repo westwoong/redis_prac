53806ea4857f94eb36e4204df0fb95d772c12d07
westwoong <skyaliceg@naver.com>
2025-07-11 11:29:43 +0900

    log 저장시 콘솔로그 추가

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -1,24 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Log } from './schema/log.schema';
 import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class LoggerService {
+    private readonly localLogger = new Logger(LoggerService.name);
     constructor(
         @InjectModel(Log.name)
         private readonly logModel: Model<Log>
     ) {}
 
     async saveLog(logData: Log) {
+        this.localLogger.log(logData);
         try {
-            const KST_TIME = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' });
+            const KST_TIME = new Date(Date.now() + 9 * 60 * 60 * 1000);
             const logDataSetup = {
                 ...logData,
                 createdAt: KST_TIME,
             }
             const log = new this.logModel(logDataSetup);
-            await log.save();
+            const result = await log.save();
+            this.localLogger.log(`mongodb 저장 성공: ${result}`);
         } catch (error) {
             console.error(error);
             return false;
