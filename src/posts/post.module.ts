0a6b1800d85e8b3898a67f414b04531b2e3539c5
westwoong <skyaliceg@naver.com>
2025-07-11 11:25:10 +0900

    로그스키마, 로그서비스 의존성

--- src/posts/post.module.ts
+++ src/posts/post.module.ts
@@ -4,12 +4,15 @@ import { PostController } from './post.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './schema/post.schema';
 import { MongoPostRepository } from './mongoPost.repository';
+import { LoggerService } from 'src/common/logger/logger.service';
+import { Log, LogSchema } from 'src/common/logger/schema/log.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),    
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    MongooseModule.forFeature([{ name: Log.name, schema: LogSchema }]),
   ],
-  providers: [PostService, MongoPostRepository],
+  providers: [PostService, MongoPostRepository, LoggerService],
   controllers: [PostController]
 })
 export class PostModule {}
